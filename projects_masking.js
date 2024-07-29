1fb7ac4f1ff7ce6e4e468fd297c76c073ef0d4d9
dharana07 <146009541+dharana07@users.noreply.github.com>
2024-07-29 16:59:47 +0530

    Update projects_masking.js

--- projects_masking.js
+++ projects_masking.js
@@ -1,1 +1,1 @@
-["pngmx", "pngbr", "gskjp", "ccaau", "beiersdorfco", "sinoth", "diageous", "pepsicoes", "sanofies", "diageotw", "beiersdorfin", "kibonbr", "pnghk", "altriaus", "munchysmy", "beiersdorfchl", "gskgs"]
+["pngmx", "pngbr", "gskjp", "ccaau", "beiersdorfco", "sinoth", "diageous", "pepsicoes", "sanofies", "diageotw", "beiersdorfin", "kibonbr", "pnghk", "altriaus", "munchysmy", "beiersdorfchl", "gsksg"]
